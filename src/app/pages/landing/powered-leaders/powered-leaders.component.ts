c4d06bbee98140b380cb033e1e0bb55e3df0a1d9
hajar benlarhzal <hajar@hajars-MacBook-Pro.local>
2025-11-20 19:19:49 +0100

    changing some pics

--- src/app/pages/landing/powered-leaders/powered-leaders.component.ts
+++ src/app/pages/landing/powered-leaders/powered-leaders.component.ts
@@ -11,17 +11,17 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
 })
 export class PoweredLeadersComponent {
   partners = [
-   
+    { link : 'https://iq.wiki/wiki/ozarus', name: 'wiki', logo: 'assets/logos/wiki.jpeg' },
       { link : 'https://www.orbler.io/', name: 'orbler', logo: 'assets/logos/orbler.svg' },
       { link : 'https://www.cogtoken.network/', name: 'cogtoken', logo: 'assets/logos/cogtoken.png' },
       { link : 'https://www.manuspay.org/', name: 'manuspay', logo: 'assets/logos/manuspay.png' },
-      { link : 'https://www.neurochain.ai/', name: 'neurochain', logo: 'assets/logos/ncn_black_logo.svg' },
+      { link : 'https://www.neurochain.ai/', name: 'neurochain', logo: 'assets/logos/neurochain.jpeg' },
       { link : 'https://omniminds.ai/', name: 'omniminds', logo: 'assets/leaders/crMDrMqL_400x400.png' },
       { link : 'https://cryfi.app/', name: 'cryfi', logo: 'assets/leaders/QMIpPKX3_400x400.png' },
       { link : 'https://vitaminai.online/', name: 'vitaminai', logo: 'assets/logos/logo-vitaminai.webp' },
       { link : 'https://xpayfi.io/', name: 'xpayfi', logo: 'assets/logos/payfi.png' },
       { link : 'https://www.chaingpt.org/', name: 'chaingpt', logo: 'assets/leaders/RMXAHHLa_400x400.png' },
-      { link : 'https://bubblemaps.io/', name: 'bubblemaps', logo: 'assets/leaders/dj1tOY36_400x400.png' },
+      { link : 'https://bubblemaps.io/', name: 'bubblemaps', logo: 'assets/leaders/dj1tOY36_400x400.jpg' },
       { link : 'https://www.tokenmetrics.com/', name: 'tokenmetrics', logo: 'assets/leaders/lXZwT_Sn_400x400.png' },
 
   ];
